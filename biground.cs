21052adc65126eab57fd6861ced6d14cf95b5c7d
puyihe <82323222+puyihe@users.noreply.github.com>
2022-11-28 16:51:13 -0800

    Add files via upload
    A big round control class, a game needs n big rounds, each big round is divided into several small rounds
    A small round control class, a small round contains many attack information

---
+++ biground.cs
@@ -0,0 +1,74 @@
+﻿constructor(data: WaveData) {
+    super();
+    this._data = data;
+    this._rounds = this._data.getRounds();//回合信息，是一个数组,长度代表回合的此时
+    this.index = 0;//大回合进行的索引
+    ......
+}
+//检查是否要更新大回合
+private checkRound()
+{
+    if (this._round == null)
+    {
+        //更新大回合
+        this._round = new LoopRound(this._rounds[this.index - 1])
+        if (this.index != 1)  
+        {
+            
+            
+        }
+    }
+}
+public update(f: number) {
+    // console.log("LoopWave:update", this.index);
+    if (this.index > this._rounds.length)
+    {
+        //所有的大回合结束了
+        console.log("LoopWave:update round end");
+        if (this.checkUnitIdle())
+        {
+            this.isFinish = true
+          }
+        else
+        {
+            if (!this._checkEndIdle)
+            {
+                BuffManager.getBuffManager().engine.makeUnitIdle();
+                this._checkEndIdle = true;
+            }
+        }
+    }
+    else
+    {
+        this.checkRound()
+          if (this._round)
+        {
+            //当前大回合是否开始
+            if (!this._round.isStart)
+                //开始当前大回合
+                this._round.start()
+              else
+                //更新当前这个大回合
+                this._round.update(f)
+              if (this._round.isFinish)
+            {
+                //当前这个大回合完成了
+                this._round.onFinish()
+                  this._round = null
+                  //更新大回合进行的索引
+                  this.index = this.index + 1
+                  console.log("LoopWave round next:", this.index);
+            }
+        }
+    }
+
+    if (this._isStartEnter)
+    {
+        if (this._enterTime >= this._waitTime)
+        {
+            this._unitJumpIn()
+              this._enterTime = 0
+          }
+        this._enterTime += f;
+    }
+}
---
+++ smallround.cs
@@ -0,0 +1,57 @@
+﻿constructor(data: Round) {
+    super();
+    this._data = data;
+    this._index = 0     //attack序列号
+        this._attack = null  //每一次攻击的实例
+        this._buff = null
+        this._attackIndex = 0   //execute的attack序列号
+}
+public checkAttack()
+{
+    if (this._attack == null)
+    {
+        let attackData = this._data.attacks[this._index - 1];
+        // else if (attackData.isHistory) {
+        //     this._attack = new LoopOneAttackhistory(attackData, this._index);
+        // }
+        else
+        {
+            this._attack = new LoopOneAttack(attackData, this._index);
+        }
+    }
+}
+
+public update(f: number) {
+    // console.log("LoopRpund update:", this.index, this.data.attacks.length);
+    if (this._index > this._data.attacks.length)
+    {
+        //大回合所有攻击结束
+        this.isFinish = true;
+    }
+    else
+    {
+        //判断是否要进入新的攻击
+        this.checkAttack();
+        if (this._attack)
+        {
+            if (this._attack.isFinish)
+            {
+                //本轮攻击结束 清除本轮攻击数据
+                this._attack.clear();
+
+                this._attack = null;
+                //攻击索引+1 准备进入下一次攻击
+                this._index += 1;
+                console.log("LoopRound attack next:", this._index);
+            }
+            else
+            {
+                if (this._attack.isExecute())
+                {
+                    this._attack.execute();
+                    this._attackIndex += 1;
+                }
+            }
+        }
+    }
+}
